cc84e242ccc73b12c66d8166432833c2302f5537
DmityFilchev <filchev_dmitry@mail.ru>
2022-09-12 13:42:20 +0300

    Добавьте файлы проекта.

---
+++ Task22_1/Task22_1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include<string>
+#include<map>
+using namespace std;
+
+int main()
+{
+	int number = 0;
+	bool exit = true;
+	string phone;
+	string names_phone;
+	map <string, string> call;
+	map <string, string> name;
+	while (exit)
+	{
+		cout << "What do you want to do :\n1 - add a new contact,\n2 - find out the name of the subscriber,\n3 - find out the phone number\n4-exit\n";
+		cin >> number;
+		if (number == 1)
+		{
+			cout << "Enter your phone number : ";
+			cin >> phone;
+			cout << "Enter name : ";
+			cin >> names_phone;
+			call.insert(pair<string, string>(phone, names_phone));
+		}
+		else if (number == 2)
+		{
+			cout << "Enter your phone number : ";
+			cin >> phone;
+			for (auto i : call)
+			{
+				if (i.first == phone)
+					cout << i.second << endl;
+			}
+		}
+		else if (number == 3)
+		{
+			cout << "Enter name : ";
+			cin >> names_phone;
+			for (auto i : call)
+			{
+				if (i.second == names_phone)
+					cout << i.first << endl;
+			}
+		}
+		else if (number == 4)
+		{
+			exit = false;
+		}
+		else
+			cout << "Invalid input" << endl;
+	}
+}
+
