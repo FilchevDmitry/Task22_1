db6025daac2f15cfedc7c7e0cbf3db599c033613
DmityFilchev <filchev_dmitry@mail.ru>
2022-09-12 16:35:03 +0300

    '12-01'

--- Task22_1/Task22_1.cpp
+++ Task22_1/Task22_1.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include<string>
+#include<vector>
 #include<map>
 using namespace std;
 
@@ -9,8 +10,9 @@ int main()
 	bool exit = true;
 	string phone;
 	string names_phone;
+	map <string, vector <string>> name;
 	map <string, string> call;
-	map <string, string> name;
+
 	while (exit)
 	{
 		cout << "\nWhat do you want to do :\n1 - add a new contact,\n2 - find out the name of the subscriber,\n3 - find out the phone number\n4-exit\n";
@@ -21,26 +23,40 @@ int main()
 			cin >> phone;
 			cout << "Enter name : ";
 			cin >> names_phone;
-			call.insert(pair<string, string>(phone, names_phone));
+			if (name.find(names_phone) != name.end())
+			{
+				name[names_phone].push_back(phone);
+				call.insert(pair<string, string>(phone, names_phone));
+			}
+			else
+			{
+				call.insert(pair<string, string>(phone, names_phone));
+				name[names_phone].push_back(phone);
+			}
+			
 		}
 		else if (number == 2)
 		{
 			cout << "\nEnter your phone number : ";
 			cin >> phone;
-			for (auto i : call)
+			for (auto i :call)
 			{
 				if (i.first == phone)
-					cout << i.second << endl;
+					cout << "Name - " << i.second << endl;
 			}
+		
 		}
 		else if (number == 3)
 		{
 			cout << "\nEnter name : ";
 			cin >> names_phone;
-			for (auto i : call)
+			for (auto i : name)
 			{
-				if (i.second == names_phone)
-					cout << i.first <<" "<<i.second<< endl;
+				if (i.first == names_phone)
+				{
+					for(auto n : name[names_phone])
+					cout << n << endl;
+				}
 			}
 		}
 		else if (number == 4)
